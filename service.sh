8366fb6c31061a063338c821cd1c2e3081a0a523
Azunyan1111 <admin@mail.azunyan1111.com>
2022-10-26 17:15:46 +0900

    add gesture sens

--- service.sh
+++ service.sh
@@ -5,4 +5,9 @@
 # This will make sure your module will still work
 # if Magisk change its mount point in the future
 
+# camera
 setprop audio.camerasound.force false
+
+# gesture sens
+settings put secure back_gesture_inset_scale_left 1.6
+settings put secure back_gesture_inset_scale_right 1.6
